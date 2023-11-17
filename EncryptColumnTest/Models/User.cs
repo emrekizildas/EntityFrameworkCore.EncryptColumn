ea125bf90e3970efb99864572c2f4b9c15cdfa41
John Dubois <john.dubois@Foundever.com>
2023-11-17 15:07:36 -0500

    Now targets .net 6, 7, and 8. Added unit tests.

---
+++ EncryptColumnTest/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using EntityFrameworkCore.EncryptColumn.Attribute;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptColumnTest.Models;
+
+public class User
+  {
+  [Key]
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+  public Guid? ID { get; set; }
+  public string? Firstname { get; set; }
+  public string? Lastname { get; set; }
+  [EncryptColumn]
+  public string? EmailAddress { get; set; }
+  [EncryptColumn]
+  public string? IdentityNumber { get; set; }
+  }
